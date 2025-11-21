6009cebf993eda97fecf694eb037f592755692f0
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2025-11-21 19:00:41 +0530

    add styling and components for enhanced UI and state management examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import './App.css'
 import ReactSetupGuide from "./components/concepts/introduction/ReactSetupGuide";
+import ReactClassStateGuide from "./components/concepts/States/ReactClassState";
+import ReactPropsGuide from "./components/concepts/props/ReactPropsGuide";
+import ReactStateGuide from "./components/concepts/States/ReactStateGuide";
+import Counter from "./components/concepts/States/Counter";
+import ReactComponentsGuide from "./components/concepts/compTypes/ReactComponentGuide";
 export default function App(){
   return(
-    <div className="bg-gray-300">
+    <div className="app-container">
       <h1 className="text-3xl font-bold bg-blue-500 text-white p-4 rounded text-center">Hello world</h1>
-      <ReactSetupGuide></ReactSetupGuide>
+      <div className="component-block">
+                <ReactSetupGuide />
+      </div>
+
+      <div className="component-block">
+         <ReactClassStateGuide />
+      </div>
+
+      <div className="component-block">
+            <ReactPropsGuide/>
+      </div>
+       <div className="component-block">
+          <ReactStateGuide />
+          <Counter/>
+          <ReactComponentsGuide/>
+      </div>
+       <div className="component-block">
+
+      </div>
     </div>
   )
 }
